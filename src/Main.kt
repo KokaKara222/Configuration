bfde9c670c0fc773b6f0f6e2c6b4f981e9f65081
Kara July <karamellem15@gmail.com>
2026-09-19 14:26:08 +0300

    Этап 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.File
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun findCommand(input: String): Boolean {
+    val parts = input.split("\\s+".toRegex())
+    val command = parts[0]
+    val args = parts.drop(1)
+    return when (command){
+        "ls" -> {
+            println("ls ${args.joinToString(" ")}")
+            true
+        }
+        "cd" -> {
+            println("cd ${args.joinToString(" ")}")
+            true
+        }
+        "exit" -> true
+        else -> {
+            println("Unknown command: $command")
+            false
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    var vfsPart: String? = null
+    var scrPart: String? = null
+
+    for (i in args.indices) {
+        if (args[i] == "--vfs" && i+1 < args.size) {
+            vfsPart = args[i+1]
+        }
+        if (args[i] == "--script" && i+1 < args.size) {
+            scrPart = args[i+1]
+        }
+    }
+    println("[DEBUG] VFS path: ${vfsPart ?: "не задан"}")
+    println("[DEBUG] Script path: ${scrPart ?: "не задан"}")
+
+    if (scrPart != null) {
+        val file = File(scrPart)
+        if (file.exists()) {
+            val lines = file.readLines()
+            for (line in lines) {
+                val trimmed = line.trim()
+                if (trimmed.isEmpty()) continue
+                println("vrs> $trimmed")
+
+                if (trimmed == "exit") continue
+
+                val command = findCommand(trimmed)
+                if (!command){
+                    println("[ERROR] Invalid command: $command")
+            }
+        }
+        } else println("[ERROR] Файл не найден")
+    }
+
+    while (true) {
+        print("vfs> ")
+        val input = readlnOrNull()?.trim()
+
+        if (input == "exit") break
+        else if (input.isNullOrEmpty()) continue
+
+        findCommand(input)
+
     }
 }
